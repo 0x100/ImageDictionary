385b159afba6167e6b26eec859182e418b56013c
Ilya Lysenko <lysenko.ilya@gmail.com>
2013-08-15 14:15:54 +0400

    Added Yandex logo in popup container

--- js/content.js
+++ js/content.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+﻿chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.method == "getSelection") {
         sendResponse({data: window.getSelection().toString()});
     }
@@ -53,11 +53,31 @@ function createDialog() {
     dialog.title = "Image Dictionary";
     dialog.style.display = "none";
     dialog.className = "img-dialog";
+    
+    var logo = document.createElement("div");    
+    logo.id = 'yandex-logo';
+    logo.className = "yandex-logo";
+        
+    var logoHref = document.createElement("a");    
+    logoHref.href = "http://images.yandex.ru";    
+    
+    var logoImg = document.createElement("img");
+    logoImg.src = "http://img.yandex.net/i/m_logo.png";
+    logoImg.alt = "";
 
     var dialogContent = document.createElement("p");
     dialogContent.id = "dialogContent";
     dialogContent.className = "img-dialog-content";
+    
+    var resultsCount = document.createElement("span");    
+    resultsCount.id = "resultsCount";    
+    resultsCount.className = "results";    
 
+    logoHref.appendChild(logoImg);
+    logo.appendChild(logoHref);
+    logo.appendChild(resultsCount);
+    dialog.appendChild(logo);
+    
     dialog.appendChild(dialogContent);
     document.body.appendChild(dialog);
 
@@ -80,15 +100,21 @@ function getData(selectedText) {
 
             var doc = xmlhttp.responseText;
             var hrefs = doc.match(/http:\/\/[\w-]+.yandex.net\/i\?id=[\w-]+/g); //Matches to the url like "http://im0-tub-ru.yandex.net/i?id=473594863-10-71"
+            var resultsCount = doc.match(/[\d]+\-[\d]+ [а-яa-z]+ [\d]+&nbsp;[а-яa-z]+./g); //Matches to string like "1-12 из 199 тыс."
             var result = '';
             
             if(hrefs && hrefs.length > 0) {
                 for(var i = 0; i < hrefs.length && i < 12; i++) {
                     result += "<div class='imgbox'>";
-                    result += "<img src='" + hrefs[i] + "' class='img' alt='' />";
+                    result += "<a href=''><img src='" + hrefs[i] + "' alt='' /></a>";
                     result += "</div>";
                 }
             }            
+            console.log(resultsCount);
+            if(resultsCount) {
+              $("#resultsCount").text("&nbsp;" + resultsCount);
+            }
+              
             $("#dialogContent").html(result);
         }
     };
